f15b1591044b0328b36abdfec7c28abc275c93f8
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2017-03-04 21:30:58 -0800

    Made the npc pace back and forth randomly

--- RolePlayingGames/NonPlayerCharacter.swift
+++ RolePlayingGames/NonPlayerCharacter.swift
@@ -11,11 +11,57 @@ import Foundation
 import SpriteKit
 
 class NonPlayerCharacter: SKSpriteNode {
-    
+    var frontName:String = " "
+    var backName:String = " "
+    var leftName:String = " "
+    var rightName:String = " "
     func setupWithDict( theDict: [String: Any] ){
-        print("Setup custom class NPC")
-        //print( theDict)
+    
+        for (key, value) in theDict
+        {
+            if (key == "Front") {
+                frontName = value as! String
+                //self.run(SKAction(named: frontName)!)
+            } else if (key == "Back") {
+                backName = value as! String
+            } else if (key == "Left") {
+                leftName = value as! String
+            } else if (key == "Right") {
+                rightName = value as! String
+            }
+        }
+        
+        walkRandom()
     }
     
-
+    func walkRandom() {
+        let waitTime = arc4random_uniform(3)
+        let wait:SKAction = SKAction.wait(forDuration: TimeInterval(waitTime))
+        let endMove:SKAction = SKAction.run {
+            self.walkRandom()
+        }
+        let randomNum = arc4random_uniform(4)
+        
+        if (randomNum == 0) {
+            self.run(SKAction(named: frontName)! )
+            let move:SKAction = SKAction.moveBy(x: 0, y: -50, duration: 1)
+            self.run(SKAction.sequence([move, wait, endMove]))
+            
+        } else if (randomNum == 0) {
+            self.run(SKAction(named: backName)! )
+            let move:SKAction = SKAction.moveBy(x: 0, y: 50, duration: 1)
+            self.run(SKAction.sequence([move, wait, endMove]))
+        } else if (randomNum == 0) {
+            self.run(SKAction(named: leftName)! )
+            let move:SKAction = SKAction.moveBy(x: -50, y: 0, duration: 1)
+            self.run(SKAction.sequence([move, wait, endMove]))
+        } else {
+            self.run(SKAction(named: rightName)! )
+            let move:SKAction = SKAction.moveBy(x: 50, y: 0, duration: 1)
+            self.run(SKAction.sequence([move, wait, endMove]))
+        }
+        
+        
+        
+    }
 }
